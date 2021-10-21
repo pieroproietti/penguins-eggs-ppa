139d1954b845d72136343fef1aa0450a8f5fed39
Piero Proietti <piero.proietti@gmail.com>
2021-10-21 16:17:46 +0200

    8.17.4

---
+++ publish.sh
@@ -0,0 +1,5 @@
+dpkg-scanpackages --multiversion . > Packages
+gzip -k -f Packages
+apt-ftparchive release . > Release
+gpg --default-key piero.proietti@gmail.com -abs -o - Release > Release.gpg
+gpg --default-key piero.proietti@gmail.com  --clearsign -o - Release > InRelease
