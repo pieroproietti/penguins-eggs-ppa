3d4d3a81f4456507ae38d2f445d474782ff27f11
Piero Proietti <piero.proietti@gmail.com>
2025-04-04 07:10:26 +0200

    vediamo se va

--- publish.sh
+++ publish.sh
@@ -3,6 +3,3 @@ gzip -k -f Packages
 apt-ftparchive release . > Release
 gpg --default-key piero.proietti@gmail.com -abs -o - Release > Release.gpg
 gpg --default-key piero.proietti@gmail.com  --clearsign -o - Release > InRelease
-#apt-ftparchive release . > Release
-#gpg --default-key piero.proietti@gmail.com -abs -o Release.gpg Release
-#gpg --default-key piero.proietti@gmail.com --clearsign -o InRelease Release
