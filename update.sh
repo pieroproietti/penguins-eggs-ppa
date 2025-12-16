755fe387bb2d5e5ca594a270960f0c15104b1715
Piero Proietti <piero.proietti@gmail.com>
2025-12-16 17:34:45 +0100

    v25.12.16-2

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-LAST_RELEASE="25.12.16-1"
+LAST_RELEASE="25.12.16-2"
 URL_BASE="https://penguins-eggs.net/basket/packages/debs/"
 
-amd64=("penguins-eggs_${LAST_RELEASE}-1_amd64.deb")
-arm64=("penguins-eggs_${LAST_RELEASE}-1_arm64.deb")
-i386=("penguins-eggs_${LAST_RELEASE}-1_i386.deb")
+amd64=("penguins-eggs_${LAST_RELEASE}_amd64.deb")
+arm64=("penguins-eggs_${LAST_RELEASE}_arm64.deb")
+i386=("penguins-eggs_${LAST_RELEASE}_i386.deb")
 
 wget -q -O "dists/amd64/$amd64" "$URL_BASE/$amd64"
 wget -q -O "dists/arm64/$arm64" "$URL_BASE/$arm64"
