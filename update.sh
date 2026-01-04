2455cd10796aae09b9bc00634d4fcfd5f3fe1d21
Piero Proietti <piero.proietti@gmail.com>
2026-01-04 09:38:12 +0100

    v26.1.3-1 added riscv64 architecture

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-LAST_RELEASE="25.12.16-6"
+LAST_RELEASE="26.1.3-1"
 URL_BASE="https://penguins-eggs.net/basket/packages/debs/"
 
 amd64=("penguins-eggs_${LAST_RELEASE}_amd64.deb")
 arm64=("penguins-eggs_${LAST_RELEASE}_arm64.deb")
 i386=("penguins-eggs_${LAST_RELEASE}_i386.deb")
+riscv64=("penguins-eggs_${LAST_RELEASE}_riscv64.deb")
 
 wget -q -O "dists/amd64/$amd64" "$URL_BASE/$amd64"
 wget -q -O "dists/arm64/$arm64" "$URL_BASE/$arm64"
 wget -q -O "dists/i386/$i386" "$URL_BASE/$i386"
+wget -q -O "dists/riscv64/$riscv64" "$URL_BASE/$riscv64"
+
